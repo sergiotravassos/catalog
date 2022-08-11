9e2a3cfe1776c2df03f6a6fe4ccbbff20fb14d5a
sergiotravassos <sergiotravassos@outlook.com>
2022-08-11 16:35:03 +0100

    Created entity User and Role

--- backend/src/main/java/com/sergiotravassos/catalog/entities/Category.java
+++ backend/src/main/java/com/sergiotravassos/catalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.sergiotravassos.catalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable{
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -72,7 +78,11 @@ public class Category implements Serializable{
 	public void preUpdate() {
 		updatedAt = Instant.now();
 	}
-	
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
